83a0f88b147df0314bc010ed24d8393b33e80649
Daniel Romero <130934263+Daniromero1410@users.noreply.github.com>
2024-10-23 13:14:03 -0500

    Agregamos el contenido de Menu de usuario

--- app-riga/app/components/navbar/UserMenu.tsx
+++ app-riga/app/components/navbar/UserMenu.tsx
@@ -1,11 +1,87 @@
-'use client'
+'use client';
+
+import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
-    return(
-       <div className="relative">
+    const[isOpen, setIsOpen] = useState(false);
 
-       </div> 
+    const toggleOpen= useCallback(()=>{
+        setIsOpen((value) => !value);
+
+    }, []);
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <button 
+                    onClick={() => {}}
+                    className="
+                        hidden
+                        md:block
+                        text-sm
+                        font-semibold
+                        py-3
+                        px-4
+                        rounded-full
+                        hover:bg-neutral-100
+                        transition
+                        cursor-pointer
+                    "
+                >
+                    Patahouse tu casa
+                </button>
+                <button
+                    onClick={toggleOpen}
+                    className="
+                        p-4
+                        md:py-2
+                        md:px-4
+                        border-[1px]
+                        border-neutral-200
+                        flex
+                        flex-row
+                        items-center
+                        gap-3
+                        rounded-full
+                        cursor-pointer
+                        hover:shadow-md
+                        transition
+                    "
+                >
+                    <AiOutlineMenu size={18} />
+                    <div className="hidden md:block">
+                        <Avatar />
+                    </div>
+                </button>
+            </div>
+            {isOpen && (
+               <div
+               className="
+               absolute
+               rounded-xl
+               shadow-md
+               w-[40vw]
+               md:w-3/4
+               bg-white
+               overflow-hidden
+               right-0
+               top-12
+               text-sm
+               ">
+                <div className="flex flex-col cursor-pointer">
+                    <>
+                        <MenuItem
+                        onClick={() => {}}
+                        label ="Inicio de Sesión"
+                        />
+                    </>
+                </div>
+               </div> 
+            )}
+        </div>
     );
 }
 
-export default UserMenu
+export default UserMenu;
